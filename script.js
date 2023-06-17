516646e62708037c067995e2e84c525b7e2ce63c
tomimg <tomasmiranda466@gmail.com>
2023-06-17 18:32:05 -0300

    Initial commit

---
+++ script.js
@@ -0,0 +1,14 @@
+window.addEventListener('DOMContentLoaded', () => {
+    VANTA.FOG({
+        el: "#vanta",
+        mouseControls: true,
+        touchControls: true,
+        gyroControls: false,
+        minHeight: 200.00,
+        minWidth: 200.00
+    })
+ });
+
+
+
+
